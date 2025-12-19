fcd70a4ca290e0701ae6ceb8c76b19a6399ea991
fangao <fangao@staruniongame.com>
2025-12-19 11:59:17 +0800

    Embed proto files to ensure they are included in vendor

---
+++ proto_embed.go
@@ -0,0 +1,12 @@
+// Package quickgo ensures proto files are included in vendor
+package quickgo
+
+import (
+	_ "embed"
+)
+
+// This variable ensures the proto file is embedded in the binary,
+// which forces Go to include it in vendor directories.
+//
+//go:embed grpcep/lib.proto
+var libProto []byte
